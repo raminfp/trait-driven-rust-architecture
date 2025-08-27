6ea71acd6dcad1ce665e30671a3f3af070ea9edb
raminfp <ramin.blackhat@gmail.com>
2025-08-27 13:26:12 +0330

    first commit

--- bin/cli.rs
+++ bin/cli.rs
@@ -1,6 +1,8 @@
 #![cfg(feature = "cli")]
 use traity_app::prelude::*;
 use traity_app::{User, Vault};
+use traity_app::traits::serialize::MySerialize;
+use std::io::Cursor;
 
 fn main() -> Result<()> {
     let user = User::new(1, "ramin", Some("ramin@example.com"))?;
@@ -9,13 +11,17 @@ fn main() -> Result<()> {
     vault.activate();
     vault.deposit(25000)?;
 
+    // let mut vault = Vault::new(1, 100);
+    let mut buffer = Vec::new();
+    let mut cursor = Cursor::new(&mut buffer);
 
+    vault.try_serialize(&mut cursor)?;
+    println!("{:?}", vault);
     let _ = vault.withdraw(user.id, 5000)?;
 
     let mut buf = Vec::new();
     #[allow(unused_mut)]
-    let mut writer = std::io::Cursor::new(&mut buf);
-    use traity_app::traits::serialize::MySerialize;
+    let mut writer = Cursor::new(&mut buf);
     vault.try_serialize(&mut writer)?;
 
     println!("ok: balance={}", vault.balance);
